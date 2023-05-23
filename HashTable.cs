621b4b9d1ceed953b0149bcd6d9ff0b386c2eb96
Asbjorn Lorenzen <asbjorn.lorenzen@gmail.com>
2023-05-23 12:26:32 +0200

    Implemented assignment 2 a,b,c

--- HashTable.cs
+++ HashTable.cs
@@ -1,25 +1,66 @@
 using HashFunctions;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace HashFunctions {
+    using keyval = Tuple <BigInteger,long>;
+    
     public class HashTable {
-        public HashTable(int tableSize, ) {
+        LinkedList<keyval>[] table;
+        Hashing hasher;
+        long size;
+        uint l;
+        public HashTable(uint l) {
             //Create array for all lists
-            int[] array1 = new int[tableSize];
+            this.l = l;
+            this.size = 1 << (int) (l + 1);
+            this.hasher = new Hashing();
+            this.table = new LinkedList<keyval>[size];
+
+            // Instantiate linked lists:
+            for (int i = 0; i < size; i++) {
+                table[i] = new LinkedList<keyval>();
+            }
         }
         
-        public int get(Int64 x) {
-            return array1[x];
+        public long get(BigInteger x) {
+            ulong key = this.hasher.MultiplyModPrime(x,this.l);
+            foreach (var val in this.table[key]) {
+                if (val.Item1 == x) {
+                    return val.Item2;
+                }
+            }
+            return 0;
         }
-        public void set(Int64 x, int v){
+
+        public void set(BigInteger x, long v){
+            ulong key = this.hasher.MultiplyModPrime(x,this.l);
+            keyval tuple = new keyval(x,v);
             
-            List<int> listEmpty = new List<int>();
-            array1[] = 
-            return 
+            // remove if x already in list
+            foreach (keyval val in this.table[key]) {
+                if (val.Item1 == x) {
+                    this.table[key].Remove(val);
+                    break;
+                }
+            }
+            this.table[key].AddFirst(tuple);
         }
 
-        public void increment(Int64 x, int d){
+        public void increment(BigInteger x, long d){
+            ulong key = this.hasher.MultiplyModPrime(x,this.l);
+            keyval newTuple = new keyval(x,d);
 
+            foreach (keyval val in this.table[key]) {
+                if (val.Item1 == x) {
+                    newTuple = new keyval(x,val.Item2 + d);
+                    this.table[key].Remove(val);
+                    break;
+                }
+            }
+
+            this.table[key].AddFirst(newTuple);
+            return;
         }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@
             long a = hasher.MultiplyShiftHash(1,20);
             Console.WriteLine(a);
             hasher.MultiplyModPrime(5,20);
-            hasher.CompareRunningTime(10000000);
+            hasher.CompareRunningTime(1000000);
+
+            HashTable t = new HashTable(20);
+            t.set(10000,10);
+            t.set(20000,20);
+            t.set(30000,30);
+
+            t.increment(10000,100);
+
+            long x = t.get(10000);
+            long y = t.get(20000);
+            long z = t.get(30000);
+
+            Console.WriteLine("Read x: {0} y: {1} z: {2}",x,y,z);
         }
     }
 }

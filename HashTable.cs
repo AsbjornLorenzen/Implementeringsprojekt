9cdc53efd3bfb3672468e7ffd88041e5787f637b
ValdemarJacobi <hcl907@alumni.ku.dk>
2023-05-23 10:20:50 +0200

    Started HashTable

---
+++ HashTable.cs
@@ -0,0 +1,25 @@
+using HashFunctions;
+
+namespace HashFunctions {
+    public class HashTable {
+        public HashTable(int tableSize, ) {
+            //Create array for all lists
+            int[] array1 = new int[tableSize];
+        }
+        
+        public int get(Int64 x) {
+            return array1[x];
+        }
+        public void set(Int64 x, int v){
+            
+            List<int> listEmpty = new List<int>();
+            array1[] = 
+            return 
+        }
+
+        public void increment(Int64 x, int d){
+
+        }
+
+    }
+}
